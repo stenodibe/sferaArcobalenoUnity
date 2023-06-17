0d949f81b79a4e2acfa97f3dd61993d032c434ad
stenodibe <38405931+stenodibe@users.noreply.github.com>
2023-06-17 14:56:06 +0200

    bozza codice movimento

---
+++ SferaArcobaleno/Assets/script/movimento sfera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public float velocità;
+    private float orizzontale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        orizzontale = Input.GetAxis("Horizontal");
+       // transform.Get(Vector3.forward * orizzontale * velocità * Time.deltaTime);
+    }
+}
